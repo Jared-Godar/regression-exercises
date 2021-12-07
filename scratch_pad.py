e21c71ff32ad2670315d326ed8df6be953ac4de5
Jared Godar <jared.godar@gmail.com>
2021-12-07 12:25:02 -0600

    Update exercises

---
+++ scratch_pad.py
@@ -0,0 +1,19 @@
+def visualize_scaled_data(scaler, scaler_name, train, feature):
+    scaled = scaler.fit_transform(train[[feature]])
+    fig = plt.figure(figsize = (12,6))
+
+    gs = plt.GridSpec(2,2)
+
+    ax1 = fig.add_subplot(gs[0, :])
+    ax2 = fig.add_subplot(gs[1,0])
+    ax3 = fig.add_subplot(gs[1,1])
+
+    ax1.scatter(train[[feature]], scaled)
+    ax1.set(xlabel = feature, ylabel = 'Scaled_' + feature, title = scaler_name)
+
+    ax2.hist(train[[feature]])
+    ax2.set(title = 'Original')
+
+    ax3.hist(scaled)
+    ax3.set(title = 'Scaled')
+    plt.tight_layout();
